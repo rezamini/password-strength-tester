f0edf4240465d3b1efe578e46bcc5c7eb7429286
Reza <reza.amini.b@gmail.com>
2023-11-14 16:40:28 +0000

    - added a new script file

---
+++ script.js
@@ -0,0 +1,3 @@
+const strengthMeter = document.getElementById("strength-meter");
+const passwordInput = document.getElementById("password-input");
+const reasonsContainer = document.getElementById("reasons");
