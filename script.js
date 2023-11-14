a08328186dac14be1478cf0fe35d96adf08a35f6
Reza <reza.amini.b@gmail.com>
2023-11-14 16:42:36 +0000

    - added a new even listener for the password input and a new method to add and return the weak password weaknesses

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 const strengthMeter = document.getElementById("strength-meter");
 const passwordInput = document.getElementById("password-input");
 const reasonsContainer = document.getElementById("reasons");
+
+passwordInput.addEventListener("input", updateStrengthMeter);
+updateStrengthMeter(); //calculate the password strength immediately based on the input current password
+
+function updateStrengthMeter() {
+    if(passwordInput.value == null) return; 
+    const weaknesses = calculatePasswordStrength(passwordInput.value);
+  }
+
+function calculatePasswordStrength(password) {
+  const weaknesses = [];
+  return weaknesses;
+}
+
