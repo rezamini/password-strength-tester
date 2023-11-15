0274bba17bb3d76f336c32035583c609075eb875
Reza <reza.amini.b@gmail.com>
2023-11-15 14:10:35 +0000

    - append and added the password weakness messages, in the reason container

--- script.js
+++ script.js
@@ -8,10 +8,19 @@ updateStrengthMeter(); //calculate the password strength immediately based on th
 function updateStrengthMeter() {
     if(passwordInput.value == null) return; 
     const weaknesses = calculatePasswordStrength(passwordInput.value);
+    console.log(weaknesses);
+
+    reasonsContainer.innerHTML = '';
+    weaknesses.forEach(weakness => {
+        if(weakness == null) return;
+
+        const messageElementDiv = document.createElement('div');
+        messageElementDiv.innerHTML = weakness.message;
+        reasonsContainer.appendChild(messageElementDiv);
+    })
   }
 
 function calculatePasswordStrength(password) {
   const weaknesses = [];
   return weaknesses;
 }
-
