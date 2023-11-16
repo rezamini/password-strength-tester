a1bb55c918842de7080809e4662ea1a76402f266
Reza <reza.amini.b@gmail.com>
2023-11-16 13:36:24 +0000

    - added a validation for password length weakness

--- script.js
+++ script.js
@@ -26,5 +26,26 @@ function updateStrengthMeter() {
 
 function calculatePasswordStrength(password) {
   const weaknesses = [];
+  weaknesses.push(lengthWeakness(password));
+
   return weaknesses;
 }
+
+function lengthWeakness(password) {
+  const length = password.length;
+
+  if (length <= 5) {
+    return {
+      message: "Your password is too short",
+      deduction: 40,
+    };
+  }
+
+  if (length <= 10) {
+    return {
+      message: "Your password could be longer",
+      deduction: 15,
+    };
+  }
+}
+
